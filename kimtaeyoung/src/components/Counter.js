8146079f515915be267bb3da1268578625064e1f
overtae <rlaxodud911@gmail.com>
2022-07-07 15:55:05 +0900

    Refactor: Counter 코드 개선

--- kimtaeyoung/src/components/Counter.js
+++ kimtaeyoung/src/components/Counter.js
@@ -1,12 +1,30 @@
 import styled from "styled-components";
-// import styled, { css } from "styled-components";
 import { useState } from "react";
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  const onClick = (num) => {
+    setCounter((current) => current + num);
+  };
+
+  return (
+    <Box>
+      <Button onClick={() => onClick(-2)}>- 2</Button>
+      <Button onClick={() => onClick(-1)}>- 1</Button>
+      <h1>{counter}</h1>
+      <Button onClick={() => onClick(1)}>+ 1</Button>
+      <Button onClick={() => onClick(2)}>+ 2</Button>
+    </Box>
+  );
+}
+
+export default Counter;
 
 const Box = styled.div`
   height: 400px;
   width: 400px;
-  border: 5px solid;
-  border-color: royalblue;
+  border: 5px solid royalblue;
   display: flex;
   align-items: center;
   justify-content: space-around;
@@ -20,19 +38,3 @@ const Button = styled.button`
   padding: 10px 15px;
   cursor: pointer;
 `;
-
-export default function Counter() {
-  const [counter, setCounter] = useState(0);
-  const onClick = (num) => {
-    setCounter((current) => current + num);
-  };
-  return (
-    <Box>
-      <Button onClick={(e) => onClick(-2, e)}>- 2</Button>
-      <Button onClick={(e) => onClick(-1, e)}>- 1</Button>
-      <h1>{counter}</h1>
-      <Button onClick={(e) => onClick(1, e)}>+ 1</Button>
-      <Button onClick={(e) => onClick(2, e)}>+ 2</Button>
-    </Box>
-  );
-}
